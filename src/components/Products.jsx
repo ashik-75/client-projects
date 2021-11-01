9cec3d1a76a6f3d3b64e7484b50677abc49c7abe
Rana <ashikex49@gmail.com>
2021-10-31 19:24:23 -1100

    jhjdfd

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -16,6 +16,8 @@ const useStyle = makeStyles({
   },
 });
 
+const image_link = "https://images.unsplash.com/photo-1518843875459-f738682238a6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dmVnZXRhYmxlc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+
 const Products = ({search,category}) => {
   const classes = useStyle();
   const {data:products,isLoading:loading,isError:error} = useGetProductsQuery();
@@ -58,9 +60,9 @@ console.log(results);
         ) : (
           results &&
           results.length > 0 &&
-          results.map((prod) => (
+          results.map((prod,index) => (
             <Grid key={prod.id} key={prod.id} item xs={12} sm={6} md={4} lg={3}>
-              <Product prod={prod} />
+              <Product prod={index<4?{...prod,image_link}:prod} />
             </Grid>
           ))
         )}
