bcb4556b60358e4014846cfa32308e2b1c284fae
Rana <ashikex49@gmail.com>
2021-12-11 20:34:48 +0600

    second

--- script.js
+++ script.js
@@ -1,49 +1,85 @@
-console.log(fetch("https://jsonplaceholder.typicode.com/todos/1"));
+async function getData() {
+  const response = await fetch("http://localhost:5000/sections");
+  const result = await response.json();
 
-async function getData(){
-    const response = await fetch("http://localhost:5000/sections")
-    const f = await response.json();
-    console.log(response);
-    console.log(f)
+  const resultArray = Object.keys(result);
+
+  console.log(resultArray);
+
+  const moreOptions = document.getElementById("more-options");
+  let ul = document.createElement("ul");
+  moreOptions.appendChild(ul);
+
+  for (let i = 0; i < resultArray.length; i++) {
+    let li = document.createElement("li");
+    let input = document.createElement("input");
+    input.setAttribute("id", `${resultArray[i]}`);
+    input.setAttribute("type", "checkbox");
+    let label = document.createElement("label");
+    label.setAttribute("for", `${resultArray[i]}`);
+    label.innerHTML = `${resultArray[i]}`;
+    li.appendChild(input);
+    li.appendChild(label);
+    ul.appendChild(li);
+  }
 }
 
 getData();
 
 const cookieStorage = {
-    getItem: (item) => {
-        const cookies = document.cookie
-            .split(';')
-            .map(cookie => cookie.split('='))
-            .reduce((acc, [key, value]) => ({ ...acc, [key.trim()]: value }), {});
-        return cookies[item];
-    },
-    setItem: (item, value) => {
-        document.cookie = `${item}=${value};`
-    }
-}
+  getItem: (item) => {
+    const cookies = document.cookie
+      .split(";")
+      .map((cookie) => cookie.split("="))
+      .reduce((acc, [key, value]) => ({ ...acc, [key.trim()]: value }), {});
+    return cookies[item];
+  },
+  setItem: (item, value) => {
+    document.cookie = `${item}=${value};`;
+  },
+};
 
 const storageType = cookieStorage;
-const consentPropertyName = 'jdc_consent';
+const consentPropertyName = "jdc_consent";
 const shouldShowPopup = () => !storageType.getItem(consentPropertyName);
 const saveToStorage = () => storageType.setItem(consentPropertyName, true);
 
-
 window.onload = () => {
+  // accept btn
+  const acceptFn = (event) => {
+    saveToStorage(storageType);
+    consentPopup.classList.add("hidden");
+  };
 
-    const acceptFn = event => {
-        saveToStorage(storageType);
-        consentPopup.classList.add('hidden');
-    }
-    const consentPopup = document.getElementById('consent-popup');
-    const acceptBtn = document.getElementById('accept');
-    acceptBtn.addEventListener('click', acceptFn);
+  // close icon
+  const denyBtnFn = (event) => {
+    consentPopup.classList.add("hidden");
+  };
 
-    console.log(consentPopup)
-    if (shouldShowPopup(storageType)) {
-        console.log("inside")
-        setTimeout(() => {
-            consentPopup.classList.remove('hidden');
-        }, 2000);
-    }
+  // close icon
+  const saveBtnFn = (event) => {
+    consentPopup.classList.add("hidden");
+  };
 
+  // close icon
+  const closeBtnFn = (event) => {
+    consentPopup.classList.add("hidden");
+  };
+
+  const consentPopup = document.getElementById("consent-popup");
+  const acceptBtn = document.getElementById("accept");
+  const denyBtn = document.getElementById("deny");
+  const saveBtn = document.getElementById("save");
+  const closeBtn = document.getElementById("close");
+
+  acceptBtn.addEventListener("click", acceptFn);
+  denyBtn.addEventListener("click", denyBtnFn);
+  saveBtn.addEventListener("click", saveBtnFn);
+  closeBtn.addEventListener("click", closeBtnFn);
+
+  if (shouldShowPopup(storageType)) {
+    setTimeout(() => {
+      consentPopup.classList.remove("hidden");
+    }, 1000);
+  }
 };
