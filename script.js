70719403c6563b8303fc3cf25c88bbd88c86c4c2
Rana <ashikex49@gmail.com>
2021-12-13 20:05:53 +0600

    another

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
+// test disable all cookies and enable only first party cookies
+
+// set cookies by fn
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = `${cname}=${cvalue};${expires};samesite=strict;secure`;
+}
+
+setCookie("age", 24, 5);
+setCookie("country", "USA", 100);
+
+// suppose play btn set third party cookie but when you load page it disabled
+const playButton = document.getElementById("play");
+playButton.addEventListener("click", () => {
+  document.cookie = "play=cookies";
+});
+
+const store = document.cookie;
+
+// block all cookies except first party cookies
+
+for (let i of store.split(";")) {
+  const data = i.trim().split("=");
+  if (data[0] === "country" || data[0] === "tech-cookie" || data[0] === "age") {
+    setCookie(data[0], data[1], 100);
+  } else {
+    setCookie(data[0], data[1], -1);
+  }
+}
+
+// end enabling and disabling cookies
+
 async function getData() {
   try {
     // put your own url(in server it doesn't work)
-    const response = await fetch("http://localhost:5000/sectionss");
+    const response = await fetch("http://localhost:5000/sections");
     const result = await response.json();
 
     let resultArray = Object.keys(result);
@@ -14,6 +48,8 @@ async function getData() {
 
       let input = document.createElement("input");
       input.setAttribute("id", `${i}`);
+      input.setAttribute("name", `${i}`);
+      input.setAttribute("value", `${i}`);
       input.setAttribute("type", "checkbox");
       let label = document.createElement("label");
       label.setAttribute("for", `${i}`);
@@ -51,7 +87,7 @@ const saveToStorage = () => storageType.setItem(consentPropertyName, true);
 window.onload = () => {
   // accept btn
   const acceptFn = (event) => {
-    saveToStorage(storageType);
+    saveToStorage(setCookie("tech-cookies", "advertising", 100));
     consentPopup.classList.add("hidden");
   };
 
@@ -62,7 +98,10 @@ window.onload = () => {
 
   // close icon
   const saveBtnFn = (event) => {
-    consentPopup.classList.add("hidden");
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
+    console.log(formData);
+    // consentPopup.classList.add("hidden");
   };
 
   // close icon
@@ -82,7 +121,6 @@ window.onload = () => {
   closeBtn.addEventListener("click", closeBtnFn);
 
   if (shouldShowPopup(storageType) && getData()) {
-    console.log(getData() ? "yes" : "no");
     setTimeout(() => {
       consentPopup.classList.remove("hidden");
     }, 1000);
